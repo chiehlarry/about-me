c860caf06090931ab0c8bb3675e28b66558820d7
chiehlarry <0719123g@gmail.com>
2026-08-04 14:28:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function scaleCanvas() {
+  const canvas = document.querySelector('.canvas');
+  const scale = Math.min(
+    window.innerWidth / 1512,
+    window.innerHeight / 904
+  );
+  canvas.style.transform = `translate(-50%, -50%) scale(${scale})`;
+}
+window.addEventListener('resize', scaleCanvas);
+scaleCanvas();
