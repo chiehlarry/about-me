cb07fab7e28a61417030accf0eb6d4ba86e255e1
chiehlarry <0719123g@gmail.com>
2026-08-06 01:47:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function scaleCanvas() {
     const canvas = document.querySelector('.canvas');
     const scale = Math.min(
-        window.innerWidth / 1512,
-        window.innerHeight / 982
+        window.innerWidth / 1920,
+        window.innerHeight / 1080
     );
-    const offsetX = (window.innerWidth - 1512 * scale) / 2;
-    const offsetY = (window.innerHeight - 982 * scale) / 2;
+    const offsetX = (window.innerWidth - 1920 * scale) / 2;
+    const offsetY = (window.innerHeight - 1080 * scale) / 2;
     canvas.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(${scale})`;
 }
 window.addEventListener('resize', scaleCanvas);
