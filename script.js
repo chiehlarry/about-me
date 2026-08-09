4b6e1d1fa5b08767cbea2f2f4d9d15fc59b15cf0
chiehlarry <0719123g@gmail.com>
2026-08-09 13:06:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ window.addEventListener('load', () => {
         document.querySelector('.paint').classList.add('fade-out');
         document.querySelector('.background-text').classList.add('fade-out');
     }, 2000); // 2000 = 頁面載入後等 2 秒才開始淡出，可自行調整
-});
 
         paint.addEventListener('transitionend', () => {
             document.querySelector('.menu-background').classList.add('in-view');
