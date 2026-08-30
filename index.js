e49bad551f225d2264217f5d6534c2eae06f52d5
chiehlarry <0719123g@gmail.com>
2026-08-30 15:28:09 +0900

    1

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function startMainAnimation(){
             menubackground.removeEventListener('transitionend', handler);}
         }, );
         
-    }, 300);// 0.3s
+    }, 3000);// 0.3s
 }
 
 const track = document.querySelector('.horizontal-track'); // 之後要把水平排列的東西包進這個容器
