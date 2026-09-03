5971d3cf4c6000e1d3f402b04ddaf70ff896e95c
chiehlarry <0719123g@gmail.com>
2026-09-03 17:46:37 +0900

    1

---
+++ ice.js
@@ -0,0 +1,38 @@
+const translations = {
+    zh: {
+         // side
+        "side-aboutme":"關於LAI的事情",
+        "side-slime":"史萊運轉",
+        "side-ice":"破冰溝溝GO",
+        "side-eraser":"腦內橡皮擦",
+        "side-toki":"偷去",
+        "side-seeyou":"大新街 LAST DANCE",
+        "side-web":"網頁作品",
+        "side-graphic":"平面作品",
+        //
+        "load": "過去も自分の糧になり、人のための糸",
+        "cover01":"包裝設計 / 傳統食物翻新",
+        "intro01":"「時來運轉」，為中文四字成語。表示運氣會不斷輪轉，只要時機到了，面對命運便能看見轉機。在此作品中，只要吃下隱含祝福的點心，就能獲得自古流傳的好運加持。將傳統點心想像成從異世界到來的「史萊姆」，吃下這些點心就如同勇者向史萊姆發起挑戰一般，為了得到隱藏其中的祝福力量，勇敢地超越敵人吧！此作品的名稱「史萊運轉」即是結合遊戲中的經典怪物「史萊姆」與隱含好運的四字成語「時來運轉」，好運史萊姆的出現，便是獲得祝福的好機會！",
+        "element01":"以此作品中最重要的概念ーー史萊姆作為發想，想像史萊姆粘膩難斷、具有延展性的狀態而製作。主體以圓角轉折與大量圓弧形設計，並在其中加入精巧圖形。以具有斜度的做字來呈現躍動感與史萊姆富有活力的模樣。",
+        "element02":"掀開上蓋以解除魔法陣",
+        "element03":"另一項ACG要素是位於包裝正上方的魔法陣，是中間的史萊姆為了保護自己免於外界傷害的關鍵要素。使用兩層的設計與印刷，只要打開上蓋，原本合一的魔法陣便會分裂而被破解。只要解開了關鍵的魔法陣，就可以堂堂正正的與裡面的史萊姆一決勝負了！",
+    },
+    en: {
+         // side
+        "side-aboutme":"Things about LAI",
+        "side-slime":"Tranditional Taiwanese snack renewal",
+        "side-ice":"Onepage adult educational site",
+        "side-eraser":"Typography & 3D-artwork",
+        "side-toki":"The path I walked pass",
+        "side-seeyou":"Project & visual design",
+        "side-web":"When I meet programming",
+        "side-graphic":"Some graphic works here",
+        //
+        "load": "Lines about drawing strength from pass and experience",
+        "cover01":"Package Design / Tranditional Taiwanese snack renewal",
+        "intro01":'The name of project "史萊運轉" is combined with "時來運轉" a madarin idiom and "slime" a monster by famous digital game series.<br><br>By regarding traditional snack as slimes from another world, just like you launch a challenge to them when eating them and finally receive the blessing after defeating enemies. "時來運轉"means luck would improve at the right time. Same as it, each traditional snack has their own blessing in.',
+        "element01":"It's inspired by slime's nature like soft and extendable.The overall design features rounded corners, curves and delicated graphics in detail. The slanted typography creates a sense of movement and energy in slime.",
+        "element02":"Open the lid and shut down the magic circle",
+        "element03":"You can see more ACG element in the lid fo this package, it's a cover slime made for protecting themselves from harm outside. Designed to double layer, when you open the top, the magic circle ontop would be broken and lost its power, it's time to face to slimes and their mystic boon! ",
+    }
+};
