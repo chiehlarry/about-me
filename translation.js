7b8ffbdbab5e751829b137c2a0253c1e0b0c1af6
chiehlarry <0719123g@gmail.com>
2026-09-03 12:02:05 +0900

    1

--- translation.js
+++ translation.js
@@ -2,8 +2,12 @@ const translations = {
     zh: {
          // side
         "side-aboutme":"關於LAI的事情",
+        "side-slime":"史萊運轉",
+        "side-ice":"破冰溝溝GO",
         "side-eraser":"腦內橡皮擦",
         "side-toki":"偷去",
+        "side-seeyou":"大新街 LAST DANCE",
+        "side-web":"網頁作品"
         "side-graphic":"平面作品",
         // about me
         "me-intro": "台灣出生。<br><br>大學畢業後開始到畫室接受繪畫訓練。<br>2024年開始以平面設計師身分活動。<br><br>自行鑽研HTML、CSS與FIGMA等網頁開發工具，也曾經手過幾個網頁設計專案。<br>接下來也想繼續活用累積至今的經驗，擴展網頁設計與前端開發相關工作的視野。"
