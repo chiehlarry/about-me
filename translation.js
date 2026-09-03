9ccad1d719eee16a4be755ab4e25fd3749531a83
chiehlarry <0719123g@gmail.com>
2026-09-03 12:52:45 +0900

    1

--- translation.js
+++ translation.js
@@ -9,6 +9,26 @@ const translations = {
         "side-seeyou":"大新街 LAST DANCE",
         "side-web":"網頁作品"
         "side-graphic":"平面作品",
+        //index
+        "index01":"原來是這樣！",
+        "index02":"這個超讚！",
+        "index03-1":"還想要",
+        "index03-2":"再做得更好！",
+        "menu-slime01":"史萊運轉",
+        "menu-slime02":"",
+        "menu-ice01":"破冰溝溝GO",
+        "menu-ice02":"",
+        "menu-eraser01":"腦內橡皮擦",
+        "menu-eraser02":"",
+        "menu-toki01":"偷去",
+        "menu-toki02":"",
+        "menu-seeyou01":"大新街 LAST DANCE",
+        "menu-seeyou02":"",
+        "menu-web01":"網頁作品",
+        "menu-web02":"",
+        "menu-graphic01":"平面作品",
+        "menu-graphic02":"",
+        "menu-aboutme":"點擊LAI來查看更多關於LAI的資訊！",
         // about me
         "me-intro": "台灣出生。<br><br>大學畢業後開始到畫室接受繪畫訓練。<br>2024年開始以平面設計師身分活動。<br><br>自行鑽研HTML、CSS與FIGMA等網頁開發工具，也曾經手過幾個網頁設計專案。<br>接下來也想繼續活用累積至今的經驗，擴展網頁設計與前端開發相關工作的視野。"
     },
@@ -22,37 +42,27 @@ const translations = {
         "side-seeyou":"Project & visual design",
         "side-web":"When I meet programming",
         "side-graphic":"Some graphic works here",
+        //index
+        "index01":"Seems it's how it comes",
+        "index02":"It's awesome!",
+        "index03-1":"Eager to",
+        "index03-2":"done everything better",
+        "menu-slime01":"SLIME OF FORTUNE",
+        "menu-slime02":"",
+        "menu-ice01":"BREAK THE ICEBERG",
+        "menu-ice02":"",
+        "menu-eraser01":"ERASER IN THE HEAD",
+        "menu-eraser02":"",
+        "menu-toki01":"TOKI",
+        "menu-toki02":"",
+        "menu-seeyou01":"FINAL HOMECOMING",
+        "menu-seeyou02":"",
+        "menu-web01":"WEB WORKS",
+        "menu-web02":"",
+        "menu-graphic01":"GRAPHIC WORKS",
+        "menu-graphic02":"",
+        "menu-aboutme":"Click LAI picture to check more information about LAI!",
         // about me
         "me-intro": "Born in Taiwan.<br><br>Started attending painting classes after graduating from university.<br>Working as a GraphicDesigner since 2024.<br><br>I'm also learning some frontend engineer skills like HTML, CSS and FIGMA by myself<br>and expecting to use the skills and experiences I have to expend my career in WEB Design and Frontend engineering."
     }
 };
-
-
-//lang-switch-system
-function setLanguage(lang) {
-    document.querySelectorAll('[data-i18n]').forEach(el => {
-        const key = el.dataset.i18n;
-        translations.ja = translations.ja || {};
-        if (!translations.ja[key]) translations.ja[key] = el.innerHTML;
-        el.innerHTML = translations[lang]?.[key] ?? ''; 
-    });
-}
-
-const langButtons = document.querySelectorAll('.lang-btn');
-const savedLang = localStorage.getItem('lang') || 'ja';
-
-function activateLangButton(lang) {
-    langButtons.forEach(b => b.classList.toggle('active', b.dataset.lang === lang));
-    document.documentElement.lang = lang; 
-    setLanguage(lang);
-}
-
-activateLangButton(savedLang);
-
-langButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const lang = btn.dataset.lang;
-        localStorage.setItem('lang', lang);
-        activateLangButton(lang);
-    });
-});
