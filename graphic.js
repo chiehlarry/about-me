092cd7b6142b88bdce94f67cc8cccdea3922ecd3
chiehlarry <0719123g@gmail.com>
2026-09-07 17:07:53 +0900

    1

---
+++ graphic.js
@@ -0,0 +1,33 @@
+const translations = {
+    zh: {
+         // side
+        "side-aboutme":"關於LAI的事情",
+        "side-slime":"史萊運轉",
+        "side-ice":"破冰溝溝GO",
+        "side-eraser":"腦內橡皮擦",
+        "side-toki":"偷去",
+        "side-seeyou":"大新街 LAST DANCE",
+        "side-web":"網頁作品",
+        "side-graphic":"平面作品",
+        //
+        "load": "千里の道も一歩から始まる、一歩ずつ積み重ねの糸",
+        "cover-title":"偷去",
+        "cover01":"樹脂 / 立體作品",
+    },
+    en: {
+         // side
+        "side-aboutme":"Things about LAI",
+        "side-slime":"Tranditional Taiwanese snack renewal",
+        "side-ice":"Onepage adult educational site",
+        "side-eraser":"Typography & 3D-artwork",
+        "side-toki":"The path I walked pass",
+        "side-seeyou":"Project & visual design",
+        "side-web":"When I meet programming",
+        "side-graphic":"Some graphic works here",
+        //
+        "load": "Lines about biginning<br>first step for a grand journey",
+        "cover-title":"TOKI",
+        "cover01":"Resin / 3D Artwork",
+        
+    }
+};
