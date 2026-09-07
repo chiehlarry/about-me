ccc643b1d77d8c5604599e357309904e8f561c9e
chiehlarry <0719123g@gmail.com>
2026-09-07 16:39:51 +0900

    1

---
+++ seeyou.js
@@ -0,0 +1,52 @@
+const translations = {
+    zh: {
+         // side
+        "side-aboutme":"關於LAI的事情",
+        "side-slime":"史萊運轉",
+        "side-ice":"破冰溝溝GO",
+        "side-eraser":"腦內橡皮擦",
+        "side-toki":"偷去",
+        "side-seeyou":"大新街 LAST DANCE",
+        "side-web":"網頁作品",
+        "side-graphic":"平面作品",
+        //
+        "load": "アージュドールに踊り、祝福を捧ぐ糸",
+        "cover01":"專案設計 / 活動視覺",
+        "intro01":"",
+        "intro02":"",
+        "intro03":"",
+        "card01":"",
+        "card02":"",
+        "card03":"",
+        "card04":"",
+        "card05":"",
+        "card06":"",
+        "list01":"",
+        "list02":"",
+    },
+    en: {
+         // side
+        "side-aboutme":"Things about LAI",
+        "side-slime":"Tranditional Taiwanese snack renewal",
+        "side-ice":"Onepage adult educational site",
+        "side-eraser":"Typography & 3D-artwork",
+        "side-toki":"The path I walked pass",
+        "side-seeyou":"Project & visual design",
+        "side-web":"When I meet programming",
+        "side-graphic":"Some graphic works here",
+        //
+        "load": "Lines about dancing and<br>wishing for the golden age",
+        "cover01":"Project Design / Event Visual",
+        "intro01":"",
+        "intro02":"",
+        "intro03":"",
+        "card01":"",
+        "card02":"",
+        "card03":"",
+        "card04":"",
+        "card05":"",
+        "card06":"",
+        "list01":"",
+        "list02":"",
+    }
+};
